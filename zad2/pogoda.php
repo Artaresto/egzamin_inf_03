96af3d628c7df3dbcd18e041b6031db7d0a8eda5
Artaresto <89027893+Artaresto@users.noreply.github.com>
2023-01-02 10:26:34 +0100

    Add files via upload

---
+++ zad2/pogoda.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prognoza pogody Wrocław</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+    <div class = "baner_lewy">
+        <img src="logo.png" alt="meteo">
+
+    </div>
+    <div class = "baner_srodek">
+        <h1>Prognoza dla Wrocławia</h1>
+    </div>
+    <div class = "baner_lewy">
+        <p><h1>Prognoza dla Wrocławia</h1></p>
+    </div>
+    <div class = "glowny">
+        <table>
+            <tr>
+                <th>Data</th>
+                <th>TEMPERATURA W NOCY</th>
+                <th>TEMPERATURA W DZIEŃ</th>
+                <th>OPADY[mm/h]</th>
+                <th>CIŚNIENIE [hPa]</th>
+            </tr>
+            
+        </table>
+    </div>
+    <div class = "lewy"></div>
+    <div class = "prawy"></div>
+    <div class = "stopka"></div>
+</body>
+</html>
